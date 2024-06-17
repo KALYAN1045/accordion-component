199f2b1ab04cbc54d667ad131367cb0851580d8c
SRINIVAS KALYAN <garuguk.ug20.cse@nitp.ac.in>
2024-06-17 20:36:54 +0530

    Created Accordion wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+function AccordionItem({ title, text, num }) {
+  return <div></div>;
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="w-[700px] my-[100px] mx-auto flex flex-col gap-[24px]">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
+
 export default function App() {
-  return <div className="text-3xl font-bold underline">Hello</div>;
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
 }
